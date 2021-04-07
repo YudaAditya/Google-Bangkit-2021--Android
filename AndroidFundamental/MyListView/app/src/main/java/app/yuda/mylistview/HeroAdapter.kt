89635cbff8d17b8a5e6c6be8c0116a1eede77e83
Yuda Aditya <yudaaditya98@gmail.com>
2021-04-08 00:32:12 +0700

    MyListView Finished

--- AndroidFundamental/MyListView/app/src/main/java/app/yuda/mylistview/HeroAdapter.kt
+++ AndroidFundamental/MyListView/app/src/main/java/app/yuda/mylistview/HeroAdapter.kt
@@ -1,26 +1,48 @@
 package app.yuda.mylistview
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
     internal var heroes = arrayListOf<Hero>()
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return heroes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return heroes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+    override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View {
+        var itemView = view
+        if (itemView == null) {
+            itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false)
+        }
+        val viewHolder = ViewHolder(itemView as View)
+        val hero = getItem(position) as Hero
+        viewHolder.bind(hero)
+        return itemView
+    }
+
+    private inner class ViewHolder internal constructor(view: View){
+        private val txtName: TextView = view.findViewById(R.id.txt_name)
+        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
+        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
+
+        internal fun bind(hero: Hero) {
+            txtName.text = hero.name
+            txtDescription.text = hero.description
+            imgPhoto.setImageResource(hero.photo)
+        }
     }
 
 }
