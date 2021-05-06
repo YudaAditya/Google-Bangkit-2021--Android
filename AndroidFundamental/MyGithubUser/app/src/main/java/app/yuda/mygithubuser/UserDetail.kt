9af8d6f6c1d9359c8c1cba764e1c5eef8d0f6151
Yuda Aditya <yudaaditya98@gmail.com>
2021-05-06 11:17:56 +0700

    updates

---
+++ AndroidFundamental/MyGithubUser/app/src/main/java/app/yuda/mygithubuser/UserDetail.kt
@@ -0,0 +1,11 @@
+package app.yuda.mygithubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
